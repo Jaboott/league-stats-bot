fcb43f7b3b84e8e47c7480ce08d464c79cae03eb
Jason <jasonxuuuu@gmail.com>
2023-08-28 23:31:13 -0700

    - Completed getMatchHistory to get a specified amount of match with specified detail - Changed toString to include summoner level

--- src/main/java/LeagueObjects/LeaguePlayer.java
+++ src/main/java/LeagueObjects/LeaguePlayer.java
@@ -5,6 +5,9 @@ import org.json.JSONObject;
 import utility.AccessApi;
 import utility.SummonerInfoGetter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeaguePlayer extends SummonerInfoGetter{
 
     private int level;
@@ -16,12 +19,14 @@ public class LeaguePlayer extends SummonerInfoGetter{
     private int normalWins;
     private int normalLosses;
     private String encryptedSummonerID;
+    private List<LeagueMatch> matches = new ArrayList<>();
 
     public LeaguePlayer(String summonerName) {
         summonerName = summonerName.replaceAll(" ", "");
         this.summonerName = summonerName;
         encryptedSummonerID = getAccountId(summonerName);
-        initPlayer((JSONArray) accessApi("https://na1.api.riotgames.com", encryptedSummonerID, "/lol/league/v4/entries/by-summoner/"));
+        level = getLevel(summonerName);
+        initPlayer((JSONArray) accessApi("https://na1.api.riotgames.com","/lol/league/v4/entries/by-summoner/", encryptedSummonerID));
     }
 
     private void initPlayer(JSONArray playerData) {
@@ -40,15 +45,24 @@ public class LeaguePlayer extends SummonerInfoGetter{
         }
     }
 
-    // Todo
-    public void getMatchHistory() {
-
+    public void getMatchHistory(int numMatchHistory, boolean detailedMatch) {
+        JSONArray recentMatches = getMatches(summonerName, numMatchHistory);
+        for (int i = 0; i < recentMatches.length(); i++) {
+            System.out.println("match:" + (i + 1));
+            String matchId = recentMatches.getString(i);
+            LeagueMatch match = new LeagueMatch(matchId);
+            if (detailedMatch) {
+                System.out.println(match.getMatch());
+            } else {
+                System.out.println(match.getPlayerStat(encryptedSummonerID));
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return "summonerID: %s \n summonerName: %s \n rank: %s, lp: %d \n rankedWins: %d, rankedLosses: %d \n normalWins: %d, normalLosses: %d, normal win rate for the past %d games: %d%%".formatted(
-                encryptedSummonerID ,summonerName, rank, leaguePoint, rankedWins, rankedLosses, normalWins, normalLosses, normalLosses + normalWins,
+        return "summonerID: %s \n summonerName: %s, summonerLevel: %d \n rank: %s, lp: %d \n rankedWins: %d, rankedLosses: %d \n normalWins: %d, normalLosses: %d, normal win rate for the past %d games: %d%%\n".formatted(
+                encryptedSummonerID ,summonerName, level, rank, leaguePoint, rankedWins, rankedLosses, normalWins, normalLosses, normalLosses + normalWins,
                 Math.round(((double) normalWins / (double)(normalLosses + normalWins)) * 100));
     }
 }
