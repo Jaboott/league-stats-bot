7dae18a72545430a09f9a90c11f51ac2ab131f60
Jason <jasonxuuuu@gmail.com>
2023-08-28 23:31:13 -0700

    - Added getMatch to show everyone's stats in a match - Added getPlayerStat to show an summoner's stat in a match

--- src/main/java/LeagueObjects/LeagueMatch.java
+++ src/main/java/LeagueObjects/LeagueMatch.java
@@ -6,31 +6,51 @@ import utility.AccessApi;
 import utility.SummonerInfoGetter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LeagueMatch extends SummonerInfoGetter{
 
-    private List<LeaguePlayer> redTeam;
-    private List<LeaguePlayer> blueTeam;
-    private String matchId;
-    private SummonerInfoGetter playerFactory = new SummonerInfoGetter();
-    private AccessApi access = new AccessApi();
+    private Map<String, PlayerRecord> wonTeam = new HashMap<>();
+    private Map<String, PlayerRecord> lostTeam = new HashMap<>();
+    private JSONObject matchData;
     // Todo: make it so it shows all the player in the game, who won, what their highest mastery are.
 
     public LeagueMatch(String matchId) {
-        this.matchId = matchId;
-        initMatch();
+        matchData = (JSONObject) accessApi("https://americas.api.riotgames.com/lol", "/match/v5/matches/", matchId);
     }
 
-    private void initMatch() {
-        JSONObject matchData = (JSONObject) accessApi("https://americas.api.riotgames.com/lol", "/match/v5/matches/", matchId);
-        JSONArray players = matchData.getJSONObject("metadata").getJSONArray("participants");
-        List<LeaguePlayer> leaguePlayerList = new ArrayList<>();
-        for (int i = 0; i < players.length(); i++) {
-            leaguePlayerList.add(new LeaguePlayer(getAccountName(players.getString(i))));
+    public LeagueMatch getMatch() {
+        JSONArray participants = matchData.getJSONObject("info").getJSONArray("participants");
+        for (int i = 0; i < participants.length(); i++) {
+            JSONObject player = participants.getJSONObject(i);
+            if (player.getBoolean("win") == true) {
+                wonTeam.put(player.getString("summonerName"), new PlayerRecord(player.getString("summonerName"), player.getInt("summonerLevel"), player.getString("championName"),
+                        player.getInt("kills"), player.getInt("deaths"), player.getInt("assists"), player.getBoolean("win")));
+            } else {
+                lostTeam.put(player.getString("summonerName"), new PlayerRecord(player.getString("summonerName"), player.getInt("summonerLevel"), player.getString("championName"),
+                        player.getInt("kills"), player.getInt("deaths"), player.getInt("assists"), player.getBoolean("win")));
+            }
         }
-        System.out.println(leaguePlayerList);
+        return this;
     }
 
+    public PlayerRecord getPlayerStat(String summonerId) {
+        JSONArray participants = matchData.getJSONObject("info").getJSONArray("participants");
+        for (int i = 0; i < participants.length(); i++) {
+            JSONObject player = participants.getJSONObject(i);
+            if (player.getString("summonerId").equals(summonerId)) {
+                return new PlayerRecord(player.getString("summonerName"), player.getInt("summonerLevel"), player.getString("championName"),
+                        player.getInt("kills"), player.getInt("deaths"), player.getInt("assists"), player.getBoolean("win"));
+            }
+        }
+        return null;
+    }
 
+    @Override
+    public String toString() {
+        return "wonTeam:" + "\n" + wonTeam + "\n" +
+                "lostTeam:" + "\n" + lostTeam + "\n";
+    }
 }
