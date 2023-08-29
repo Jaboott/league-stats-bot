be27039e2afb3cb2fe76516d3d9cce65779ab032
Jason <jasonxuuuu@gmail.com>
2023-08-28 23:31:14 -0700

    Removed unnecessary variables

--- src/main/java/utility/AccessApi.java
+++ src/main/java/utility/AccessApi.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 public class AccessApi {
 
-    private final String baseUrl = "https://na1.api.riotgames.com";
-
     public Object accessApi(String baseUrl, String endPoint, String someId, String field) {
         try {
             Object playerData = UrlReader.readJsonFromUrl
