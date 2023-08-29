aa55e428c3140aa67d73690fc21a0d20a272b336
Jason <jasonxuuuu@gmail.com>
2023-08-28 23:31:13 -0700

    Made a LeagueMatch class that contains the information for a league match

---
+++ src/main/java/utility/LeagueMatch.java
@@ -0,0 +1,16 @@
+package utility;
+
+import java.util.List;
+
+public class LeagueMatch {
+
+    private List<LeaguePlayer> redTeam;
+    private List<LeaguePlayer> blueTeam;
+    private String matchId;
+    private SummonerFactory playerFactory = new SummonerFactory();
+    // Todo: make it so it shows all the player in the game, who won, what their highest mastery are.
+
+
+
+
+}
