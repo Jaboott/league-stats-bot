6b0f990a49d2790ee8a761231468923bbb48922f
Jason <jasonxuuuu@gmail.com>
2023-08-28 23:31:13 -0700

    Added initMatch methods to get the players from the api

--- src/main/java/LeagueObjects/LeagueMatch.java
+++ src/main/java/LeagueObjects/LeagueMatch.java
@@ -1,9 +1,11 @@
 package LeagueObjects;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import utility.AccessApi;
 import utility.SummonerInfoGetter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LeagueMatch extends SummonerInfoGetter{
@@ -15,10 +17,19 @@ public class LeagueMatch extends SummonerInfoGetter{
     private AccessApi access = new AccessApi();
     // Todo: make it so it shows all the player in the game, who won, what their highest mastery are.
 
-    public JSONArray getMatches(String summonerName, int numGames) {
-        JSONArray matchList = (JSONArray) accessApi("https://americas.api.riotgames.com" ,getPuuid(summonerName),
-                "/ids?type=ranked&start=0&count=" + numGames + "&", "/lol/match/v5/matches/by-puuid/");
-        return matchList;
+    public LeagueMatch(String matchId) {
+        this.matchId = matchId;
+        initMatch();
+    }
+
+    private void initMatch() {
+        JSONObject matchData = (JSONObject) accessApi("https://americas.api.riotgames.com/lol", "/match/v5/matches/", matchId);
+        JSONArray players = matchData.getJSONObject("metadata").getJSONArray("participants");
+        List<LeaguePlayer> leaguePlayerList = new ArrayList<>();
+        for (int i = 0; i < players.length(); i++) {
+            leaguePlayerList.add(new LeaguePlayer(getAccountName(players.getString(i))));
+        }
+        System.out.println(leaguePlayerList);
     }
 
 
